2ae3f5b8e6bb3cc242feda3d04256ad5c6240603
cristianeze16 <cristian.ezequiel.hermosa@gmail.com>
2022-11-01 08:26:32 -0300

    boton ocultar

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import Flechita from './components/Flechita';
 import data from './data/datosMentores';
 import { useState } from 'react';
 function App() {
-
+  let[mostrarOcultar,setMostrarOcultar] = useState(false)
   let [numeroAcambiar,setNumeroAcambiar]= useState(0)
 
+let hide= ()=>{
 
+  setMostrarOcultar(!mostrarOcultar)
+  console.log(mostrarOcultar)
+}
   
 
   let next = ()=>{
@@ -38,28 +42,40 @@ if (numeroAcambiar>0) {
         nombre={data[numeroAcambiar].nombre}
         foto={data[numeroAcambiar].foto}
       />
-      <p className="gray flex j-center a-center">ocultar info</p>
-      <div className="edad-fecha">
-        <p className="datos white r25 edad">
-          Edad:{data[numeroAcambiar].edad} años
+      {mostrarOcultar ? (
+        <>
+          <p className="gray flex j-center a-center" onClick={hide}>
+            ocultar info
+          </p>
+
+          <div className="edad-fecha">
+            <p className="datos white r25 edad">
+              Edad:{data[numeroAcambiar].edad} años
+            </p>
+            <p className="datos white l25 fecha">
+              Fecha:{data[numeroAcambiar].nacimiento}
+            </p>
+          </div>
+          <div className="datos white flex a-center">
+            <p>Mail:{data[numeroAcambiar].mail}</p>
+          </div>
+          <CampoConArray
+            titulo="titulo"
+            datos={data[numeroAcambiar].hobbies.join(" y ")}
+          />
+          <CampoConArray
+            titulo="Comidas"
+            datos={data[numeroAcambiar].comidas.join(" y ")}
+          />
+        </>
+      ) : (
+        <p className="gray flex j-center a-center" onClick={hide}>
+          Mostrar info
         </p>
-        <p className="datos white l25 fecha">
-          Fecha:{data[numeroAcambiar].nacimiento}
-        </p>
-      </div>
-      <div className="datos white flex a-center">
-        <p>Mail:{data[numeroAcambiar].mail}</p>
-      </div>
-      <CampoConArray
-        titulo="titulo"
-        datos={data[numeroAcambiar].hobbies.join(" y ")}
-      />
-      <CampoConArray
-        titulo="Comidas"
-        datos={data[numeroAcambiar].comidas.join(" y ")}
-      />
+      )}
+
       <div className=" flex j-center a-center">
-        <Flechita verbo="Anterior" estilo="r25" click ={prev}/>
+        <Flechita verbo="Anterior" estilo="r25" click={prev} />
         <Flechita verbo="Siguiente" estilo="l25" click={next} />
       </div>
     </div>
--- src/components/NombreFoto.jsx
+++ src/components/NombreFoto.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+
 export default function NombreFoto(props) {
  let{nombre,foto}=props
  
